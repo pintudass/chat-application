83173d2276f30a30cc68d40bf4a4dd5051a5d58a
pintudass <pintudasp885@gmail.com>
2021-06-25 14:23:26 +0600

    lessson 4 source code

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -2,13 +2,22 @@
 const express = require('express')
 
 // internal imports
-const {getLogin} = require('../controller/loginController')
+const {getLogin, login, logout} = require('../controller/loginController')
+const { redirectLoggedIn } = require('../middlewares/common/checkLogin')
 const decorateHtmlResponse = require('../middlewares/common/decorateHtmlResponse')
+const { doLoginValidators, doLoginValidationHandler } = require('../middlewares/login/loginValidators')
 
 const router = express.Router()
 
+const page_title = 'Login'
 
 // Login Page
-router.get('/', decorateHtmlResponse('Login'), getLogin)
+router.get('/', decorateHtmlResponse(page_title), redirectLoggedIn, getLogin)
+
+// Process login
+router.post('/', decorateHtmlResponse(page_title), doLoginValidators, doLoginValidationHandler, login)
+
+// Process logout
+router.delete('/', logout)
 
 module.exports = router
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -3,6 +3,7 @@ const express = require('express')
 
 // internal imports
 const {getUsers, addUser, removeUser} = require('../controller/usersController')
+const { checkLogin } = require('../middlewares/common/checkLogin')
 const decorateHtmlResponse = require('../middlewares/common/decorateHtmlResponse')
 const avatarUpload = require('../middlewares/users/avatarUpload')
 const { addUserValidators, addUserValidationHandler } = require('../middlewares/users/userValidators')
@@ -10,7 +11,7 @@ const { addUserValidators, addUserValidationHandler } = require('../middlewares/
 const router = express.Router()
 
 // Users page
-router.get('/', decorateHtmlResponse('Users'), getUsers)
+router.get('/', decorateHtmlResponse('Users'), checkLogin, getUsers)
 
 // Add user 
 router.post('/', avatarUpload, addUserValidators, addUserValidationHandler, addUser)
